55717afeca43bff4f48b6e143bc2afadbff6ee64
yashaswinimullalli <yashaswinimullalli4@gmail.com>
2025-08-29 22:35:32 +0530

    update my code

--- DSA(1)/String Array and bit manipulation/string.cpp
+++ DSA(1)/String Array and bit manipulation/string.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<algorithm>
 #include<cstring>
+#include<string>
 using namespace std ;
 /*int main(){
 	char ch='a';
@@ -76,12 +77,95 @@ tolow(word,strlen(word));
 	}
 	return 0;
 }*/
+/*int main(){
+	   char str1[100]="zbc" ;
+	   char str2[100]= "abc";
+	  // str1="apna collage"; assignment of variable is not posiible for string
+	  //strcpy(str1,str2);
+	   //cout<<str1<<endl;
+	 // strcat(str1,str2);
+	  // cout<<str1<<endl;
+	  cout<< strcmp(str1,str2)<<endl;	
+	return 0;
+}*/
+
+/*#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std ;
 int main(){
-	   char str1[100] ;
-	   char str2[100]= "hello world";
-	  // str1="apna collage"; assi
-	  strcpy(str1,str2);
-	   cout<<str1<<endl;
+	string str;
+	getline(cin,str);
+	cout<<str[0];
 	return 0;
 }
+*/
+//for each loop method to get acess each character from the string
 
+/*int main(){
+	string str ="apna college!";
+	// noraml method
+	for(int i=0; i<str.length();i++){
+		cout<<str[i]<<"_";
+	}
+	cout<<"\n";
+	
+for(char ch : str){
+	cout<<ch<<",";
+}
+cout<<endl;
+	return 0;
+}*/
+
+//String member function
+
+/*int main(){
+	string str = "apna nam";
+	cout<<str.length()<<endl;// length of string 
+	cout<<str.at(3)<<endl; // the array element
+	cout<<str.substr(0,4)<<endl; //substring
+	cout<<str.find("na");//position of first found char
+	return 0;
+}*/
+// valid Anagram TC :0(n)
+/*bool isanagram(string str1,string str2){
+	if(str1.length() != str2.length()){
+		cout<<"not valid anagrams";
+		return false;
+	}
+	int count[26]={0};
+		for(int i=0; i<str1.length();i++){
+			int idx =str1[i]-'a';
+			count[idx]++;
+		}
+		int idx;
+		for(int i=0;i<str2.length();i++){
+				idx =str2[i]-'a';
+			if(count[idx] == 0){
+			cout<<"invalid anagram";
+				return false;
+			}
+         	count[idx]--;
+		}
+	
+		cout<<"valid anagram";
+		}
+
+
+int main(){
+	string str1 ="anagram";
+	string str2 ="nagaram";
+	isanagram(str1, str2);
+	return 0;
+}*/
+
+//using operators to compare the variables
+
+int main(){
+	string str = "aar";
+	string str1 ="car";
+	cout<<(str ==str1)<<endl;
+	cout<<(str > str1)<<endl;
+	cout<<(str < str1)<<endl;
+	return 0;
+}
