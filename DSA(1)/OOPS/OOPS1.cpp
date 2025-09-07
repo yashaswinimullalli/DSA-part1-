44538e7faf9936d4d5e437e67f0d9bd80b47ef97
yashaswinimullalli <yashaswinimullalli4@gmail.com>
2025-09-07 17:56:13 +0530

    update

--- DSA(1)/OOPS/OOPS1.cpp
+++ DSA(1)/OOPS/OOPS1.cpp
@@ -174,171 +174,30 @@ int main(){
 }
 */
 
-//friend function
+class car{
+		public:
+		string name;
+		string color;
+		car(string name,string color){
+			cout<<"original car";
+			this->name = name;
+			this->color=color;
+		}
+		car(car &original){
+			cout<<"copiying original to new.\n";
+			name = original.name;
+			color = original.color;
+		}
 
-/*class Employee {
-  private:
-    int salary;
-
-  public:
-    Employee(int s) {
-      salary = s;
-    }
-
-    // Declare friend function
-    friend void displaySalary(Employee emp);
-};
-
-void displaySalary(Employee emp) {
-  cout << "Salary: " << emp.salary;
+	};
+int main(){
+	car c1("maruti","white");
+	car c2(c1);// custom copy constructor
+	cout<<c2.name<<endl;
+	cout<<c2.color<<endl;
+	return 0;
 }
 
-int main() {
-  Employee myEmp(50000);
-  displaySalary(myEmp);
-  return 0;
-}*/
+//
 
 
-//Inheritence
-
-// Base class
-/*class Vehicle {
-  public:
-    string brand = "Ford";
-    void honk() {
-      cout << "Tuut, tuut! \n" ;
-    }
-};
-
-// Derived class
-class Car: public Vehicle {
-  public:
-    string model = "Mustang";
-};
-
-int main() {
-  Car myCar;
-  myCar.honk();
-  cout << myCar.brand + " " + myCar.model;
-  return 0;
-}*/
-
-//multilevel inheritence
-// Base class (parent)
-/*class MyClass {
-  public:
-    void myFunction() {
-      cout << "Some content in parent class." ;
-    }
-};
-
-// Derived class (child)
-class MyChild: public MyClass {
-};
-
-// Derived class (grandchild)
-class MyGrandChild: public MyChild {
-};
-
-int main() {
-  MyGrandChild myObj;
-  myObj.myFunction();
-  return 0;
-}*/
-
-//multiple inheritence
-// Base class
-/*class MyClass {
-  public:
-    void myFunction() {
-      cout << "Some content in parent class." ;
-    }
-};
-
-// Another base class
-class MyOtherClass {
-  public:
-    void myOtherFunction() {
-      cout << "Some content in another class." ;
-    }
-};
-
-// Derived class
-class MyChildClass: public MyClass, public MyOtherClass {
-};
-
-int main() {
-  MyChildClass myObj;
-  myObj.myFunction();
-  myObj.myOtherFunction();
-  return 0;
-}*/
-
-//protected data access
-// Base class
-/*class Employee {
-  protected: // Protected access specifier
-    int salary;
-};
-
-// Derived class
-class Programmer: public Employee {
-  public:
-    int bonus;
-    void setSalary(int s) {
-      salary = s;
-    }
-    int getSalary() {
-      return salary;
-    }
-};
-
-int main() {
-  Programmer myObj;
-  myObj.setSalary(50000);
-  myObj.bonus = 15000;
-  cout << "Salary: " << myObj.getSalary() << "\n";
-  cout << "Bonus: " << myObj.bonus << "\n";
-  return 0;
-}*/
-
-//polymorphism
-/*#include <iostream>
-#include <string>
-using namespace std;
-
-// Base class
-class Animal {
-  public:
-    void animalSound() {
-      cout << "The animal makes a sound \n";
-    }
-};
-
-// Derived class
-class Pig : public Animal {
-  public:
-    void animalSound() {
-      cout << "The pig says: wee wee \n";
-    }
-};
-
-// Derived class
-class Dog : public Animal {
-  public:
-    void animalSound() {
-      cout << "The dog says: bow wow \n";
-    }
-};
-
-int main() {
-  Animal myAnimal;
-  Pig myPig;
-  Dog myDog;
-
-  myAnimal.animalSound();
-  myPig.animalSound();
-  myDog.animalSound();
-  return 0;
-}*/
