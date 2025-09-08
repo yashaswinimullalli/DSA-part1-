b3b38d0a5352bb1c8eb6cbe4a594f93dd9abff1b
yashaswinimullalli <yashaswinimullalli4@gmail.com>
2025-09-08 18:50:41 +0530

    do changes

--- DSA(1)/OOPS/OOPS2.cpp
+++ DSA(1)/OOPS/OOPS2.cpp
@@ -2,7 +2,21 @@
 #include<vector>
 #include<algorithm>
 using namespace std ;
+//polymorphism
+class print{
+public:
+	void show(int x){
+		cout<<"int :"<<x<<endl;
+	}
+	void show(string str){
+		cout<<"string :"<<str<<endl;
+	}
+
+
+};
 int main(){
-	
+	print ob1;
+	ob1.show("apna collage");
+	ob1.show(25);
 	return 0;
 }
