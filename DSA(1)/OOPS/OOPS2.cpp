430fddefa228df9d3178c129a4cece621edccb52
yashaswinimullalli <yashaswinimullalli4@gmail.com>
2025-09-08 23:41:05 +0530

    add changes

--- DSA(1)/OOPS/OOPS2.cpp
+++ DSA(1)/OOPS/OOPS2.cpp
@@ -3,7 +3,8 @@
 #include<algorithm>
 using namespace std ;
 //polymorphism
-class print{
+//1.compile time polymorphism --> (1) function overloding 
+/*class print{
 public:
 	void show(int x){
 		cout<<"int :"<<x<<endl;
@@ -18,5 +19,35 @@ int main(){
 	print ob1;
 	ob1.show("apna collage");
 	ob1.show(25);
+	return 0;
+}*/
+
+//1.compile time polymorphism --> (1) operator overloding 
+ 
+class complex{
+	int real;
+	int img;
+	public:
+	complex(int r,int i){
+		real = r;
+		img =i;
+	}
+	void showNum(){
+		cout<<real<<"+"<<img<<"i\n";
+	}
+	void operator + (complex &c2){
+		int resreal = this->real +c2.real;
+		int resimg = this->img +c2.img;
+		complex c3(resreal,resimg);
+		cout<<"res = ";
+		c3.showNum();
+	}
+};
+int main(){
+	complex c1(1,2);
+	complex c2(3,4);
+	c1.showNum();
+	c2.showNum();
+	c1 +c2;
 	return 0;
 }
